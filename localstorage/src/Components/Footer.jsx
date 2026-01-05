c05ca71ed0d9e9230d2e61811127a90e8c5575b1
Shovam Mahato <msubham150@gmail.com>
2026-01-05 19:27:37 +0530

    Initial commit: added localstorage and mongodb versions

---
+++ localstorage/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-white text-center p-4 flex gap-2 justify-center items-center w-full'>
+        &copy; 2026
+        <h1><span className='text-green-700'> &lt;</span>Pass
+        <span className='text-green-700'>OP/&gt; </span>
+        </h1>
+          All rights reserved.
+    </div>
+  )
+}
+export default Footer
